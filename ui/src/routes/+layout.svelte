d6533192b17824eb195ecc3dc3766bb2a0e4d705
Luis Eduardo <eduardo.devop@gmail.com>
2026-05-08 19:31:19 +0000

    Add loading state to layout; display loader while initializing and render children upon completion

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -2,8 +2,41 @@
   import "../app.css";
 
   import { UiProvider } from "@varavel/ui";
+  import { Loader } from "@varavel/ui/brand";
+  import { AppLayout } from "@varavel/ui/layouts";
 
   let { children } = $props();
+
+  let initialized = $state(false);
 </script>
 
-<UiProvider>{@render children()}</UiProvider>
+{#if !initialized}
+  <div class="w-dvw h-dvh overflow-hidden flex items-center justify-center">
+    <Loader size="lg" />
+  </div>
+{:else}
+  <UiProvider>
+    <AppLayout primaryRegion="header" maxWidth="md">
+      {#snippet headerLeft()}
+        <header class="hidden desk:flex ml-2 items-center gap-4"></header>
+      {/snippet}
+
+      {#snippet headerCenter()}
+        <header class="w-full desk:hidden"></header>
+      {/snippet}
+
+      {#snippet headerRight()}
+      {/snippet}
+
+      {#snippet sidebarTop()}
+      {/snippet}
+
+      {#snippet sidebarCenter()}
+      {/snippet}
+
+      {#snippet main()}
+        {@render children()}
+      {/snippet}
+    </AppLayout>
+  </UiProvider>
+{/if}
